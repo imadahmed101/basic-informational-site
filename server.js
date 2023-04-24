6b02e939ee73477f8ae421640215f397c20976b8
Imad Ahmed <imadahmed101@gmail.com>
2023-04-24 16:14:13 -0400

    Add files for basic informational site

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile('about.html', {root: __dirname})
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile('contact-me.html', {root: __dirname})
+})
+
+app.get('/*', (req, res) => {
+    res.sendFile('404.html', {root: __dirname})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
